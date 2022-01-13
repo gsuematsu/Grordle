3a04088a55d382637f2f935b3995eef8975ca46a
Gregory Suematsu <gsuematsu28@yahoo.com>
2022-01-12 20:55:20 -0800

    Update Grordle.py

--- Grordle.py
+++ Grordle.py
@@ -9,16 +9,15 @@ Created on Wed Jan 12 19:54:28 2022
 import pandas as pd
 import numpy as np
 
-print("Hello. Welcome to Grordle.")
-num_letters = input("What letter length of words do you want? ")
+# print("Hello. Welcome to Grordle.")
+# num_letters = input("What letter length of words do you want? ")
 
-print(num_letters)
-
+num_letters = '5'
 
 word_bank = pd.read_excel('Word Bank.xlsx', 
                           sheet_name = num_letters + '-letter') 
 print (word_bank)
 
-# possible_solns = word_bank.loc(word_bank['Solution'] == 'Y') 
-word_bank['Solution']
-# print(possible_solns())
+
+possible_solns = word_bank.loc[(word_bank['Solution'] == 'Y').array, ['Word']]
+print(possible_solns)
