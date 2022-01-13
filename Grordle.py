07a819b16ba53ca111aa4cf1e529afa447660bff
Gregory Suematsu <gsuematsu28@yahoo.com>
2022-01-12 20:11:33 -0800

    Create Grordle.py

---
+++ Grordle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 12 19:54:28 2022
+
+@author: gregorysuematsu
+"""
+
+import pandas as pd
+import numpy as np
+
+print("Hello. Welcome to Grordle.")
+num_letters = input("What letter length of words do you want? ")
+
+print(num_letters)
+
